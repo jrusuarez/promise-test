a288f7dbb6733676a0d52dfee26296c6877b2eef
Nam Phan <nam.phan@newoceaninfosys.com>
2017-01-03 19:02:13 +0700

    fix callback async by await

--- promises.js
+++ promises.js
@@ -1,57 +1,55 @@
 'use strict';
-const async = require('async');
 const start = Date.now();
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var Promise = require('bluebird');
+var _ = require ('lodash');
 
 function lapse() {
   return Date.now() - start;
 }
 
-function asyncFunc1(cb) {
-  console.log('asyncFunc1 called', lapse());
-  setTimeout(function () {
-    console.log('asyncFunc1 returning', lapse());
-    cb();
-  }, 100);
+function promiseFunc1(name){
+  return new Promise(function(resolve, reject){
+    console.log('asyncFunc1 called', lapse());
+    setTimeout(function () {
+      console.log('asyncFunc1 returning', lapse());
+      resolve();
+    }, 100);
+  });
 }
 
-function asyncFunc2(cb) {
-  console.log('asyncFunc2 called', lapse());
-  setTimeout(function () {
-    console.log('asyncFunc2 returning', lapse());
-    cb();
-  }, 200);
+function promiseFunc2(name){
+  return new Promise(function(resolve, reject){
+    console.log('asyncFunc2 called', lapse());
+    setTimeout(function () {
+      console.log('asyncFunc2 returning', lapse());
+      resolve();
+    }, 100);
+  });
 }
 
-function asyncFunc3(n, cb) {
-  console.log('asyncFunc2 called', n, lapse());
-  setTimeout(function () {
-    console.log('asyncFunc2 returning', n, lapse());
-    cb(null);
-  }, n * 100);
+function promiseFunc3(n){
+  return new Promise(function(resolve, reject){
+   console.log('asyncFunc3 called', n, lapse());
+   setTimeout(function () {
+    console.log('asyncFunc3 returning', n, lapse());
+    resolve();
+  }, n*100);
+ });
 }
 
+var run = async (function () { 
+   await (promiseFunc1());
+   await (promiseFunc2());   
+   await ([promiseFunc3(1),promiseFunc3(2),promiseFunc3(3)]);
+   await ( _.map([1, 2, 3], function(i){
+     return promiseFunc3(i);
+   }));
+});
 
+run().then(function (params)
+{
+ console.log('END');
+});
 
-function run() {
-  async.waterfall(
-    [
-      function (cb) {
-        asyncFunc1(cb);
-      },
-
-      function (cb) {
-        asyncFunc2(cb);
-      },
-
-      function (cb) {
-        async.each([1, 2, 3], asyncFunc3, cb);
-      }
-    ],
-
-    function () {
-      console.log('END');
-    }
-  );
-}
-
-run();
